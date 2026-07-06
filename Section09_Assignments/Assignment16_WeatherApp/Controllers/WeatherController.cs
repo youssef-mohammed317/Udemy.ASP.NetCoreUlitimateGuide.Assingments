9eb497affb5528bd9a763a53f587bea06da5bca5
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2026-07-07 02:19:35 +0300

    solve section09_assignment16_weather app using layout

---
+++ Section09_Assignments/Assignment16_WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,61 @@
+﻿using Assignment16_WeatherApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment16_WeatherApp.Controllers;
+
+public class WeatherController : Controller
+{
+    private readonly List<CityWeather> _cityWeathers;
+    public WeatherController()
+    {
+        _cityWeathers = new List<CityWeather>
+        {
+            new CityWeather
+            {
+                CityUniqueCode = "LDN",
+                CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+                TemperatureFahrenheit = 33
+            },
+            new CityWeather
+            {
+                CityUniqueCode = "NYC",
+                CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+                TemperatureFahrenheit = 60
+            },
+            new CityWeather
+            {
+                CityUniqueCode = "PAR",
+                CityName = "Paris",
+                DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+                TemperatureFahrenheit = 82
+            }
+        };
+    }
+
+    [Route("/")]
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(_cityWeathers);
+    }
+    [Route("/weather/{cityCode}")]
+    [HttpGet]
+    public IActionResult CityWeather([FromRoute] string? cityCode)
+    {
+        if (cityCode == null)
+        {
+            return BadRequest();
+        }
+
+        var cityWeather = _cityWeathers.FirstOrDefault(city => city.CityUniqueCode?.ToLower() == cityCode?.ToLower());
+
+        if (cityWeather == null)
+        {
+            return BadRequest();
+        }
+
+        return View(cityWeather);
+    }
+}
