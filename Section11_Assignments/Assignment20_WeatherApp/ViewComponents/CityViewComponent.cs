b134f800910eb95037a1bb7f77c3a7de2dee664d
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2026-07-07 03:08:25 +0300

    solve section11_assingment20_weather app using component

---
+++ Section11_Assignments/Assignment20_WeatherApp/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Assignment20_WeatherApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment20_WeatherApp.ViewComponents;
+
+public class CityViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(CityWeather city, string link, string linkText, bool needColor)
+    {
+        if (needColor)
+        {
+
+            if (city.TemperatureFahrenheit < 44)
+            {
+                ViewBag.Color = "blue-back";
+            }
+            else if (city.TemperatureFahrenheit < 74)
+            {
+                ViewBag.Color = "green-back";
+            }
+            else
+            {
+                ViewBag.Color = "orange-back";
+            }
+        }
+        else
+        {
+            ViewBag.Color = "";
+        }
+
+        ViewBag.Link = link;
+        ViewBag.LinkText = linkText;
+
+        return View(city);
+    }
+}
